a93171bf747ae33e99d79fe25ffa38a2609b8423
wulasite <wulaiste@gmail.com>
2019-07-09 22:51:49 +0800

    add the cpu and the mem test code

--- mem_test.c
+++ mem_test.c
@@ -5,6 +5,22 @@
 
 int main(int argc, char const *argv[])
 {
-	printf(1, "mem type test start!\n" );
+	printf(1, "--------------------mem type test start!-----------------------------\n" );
+
+	int i;
+	char * p;
+
+	p = (char *)0xa0000;
+
+	for(i = 0; i <= 1000; i++){
+		*(p + i) = i&0x0f;
+	}
+	for(i = 0; i <= 1000; i++){
+		*(p + i + 1000) = (*(p + i))&0x0f;
+	}
+	for(i = 0; i <= 2000; i++){
+		*(p + i) = i&0x0f;
+	}
+	printf(1, "--------------------mem type test end!-----------------------------\n" );
 	exit();
 }
