17cbeed498b97bc11633f1cfafbb7a84cdf73608
wulasite <wulaiste@gmail.com>
2019-07-15 21:22:11 +0800

    add the cfs scheduler

--- proc.c
+++ proc.c
@@ -25,6 +25,35 @@ extern void trapret(void);
 
 static void wakeup1(void *chan);
 
+static const int prio_to_weight[40] = {
+ /* -20 */ 88761, 71755, 56483, 46273, 36291,
+ /* -15 */ 29154, 23254, 18705, 14949, 11916,
+ /* -10 */ 9548, 7620, 6100, 4904, 3906,
+ /* -5 */ 3121, 2501, 1991, 1586, 1277,
+ /* 0 */ 1024, 820, 655, 526, 423,
+ /* 5 */ 335, 272, 215, 172, 137,
+ /* 10 */ 110, 87, 70, 56, 45,
+ /* 15 */ 36, 29, 23, 18, 15,
+ };
+
+static void set_load_weight(struct task_struct *p)
+{
+    int prio = p->static_prio - MAX_RT_PRIO;
+    struct load_weight *load = &p->se.load;
+ 
+    /*
+     * SCHED_IDLE tasks get minimal weight:
+     */
+    if (p->policy == SCHED_IDLE) {
+        load->weight = scale_load(WEIGHT_IDLEPRIO);
+        load->inv_weight = WMULT_IDLEPRIO;
+        return;
+    }
+ 
+    load->weight = scale_load(prio_to_weight[prio]);
+    load->inv_weight = prio_to_wmult[prio];      //权重跟进程nice值之间有一一对应的关系
+}
+
 void
 pinit(void)
 {
